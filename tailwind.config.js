d5b2e9b2860960a06754577d53329b7003c0133e
Tristan Soriaga <tristan_soriaga@apple.com>
2020-05-15 17:04:48 +0800

    Initial commit for create-react-app with tailwind and purgecss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: {
+    //remove unused style every build
+    //enabled: true,
+    content: ['./src/**/*.html', './src/**/*.js'],
+  },
+  theme: {
+    //Customize example
+    // screens: {
+    //   xsm: '340px',
+    //   sm: '640px',
+    //   md: '768px',
+    //   lg: '1024px',
+    //   xl: '1280px',
+    // },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
